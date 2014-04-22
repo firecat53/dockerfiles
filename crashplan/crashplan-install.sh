6060ab7fe774442c2b6f0c6a2b4c3aff7bc0dfff
Scott Hansen <firecat4153@gmail.com>
2014-04-21 21:53:42 -0700

    Working CrashPlan container.  The program actually gets installed to a data-only volume, not inside the image.

--- crashplan/crashplan-install.sh
+++ crashplan/crashplan-install.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 ## Thanks to the maintainer of https://aur.archlinux.org/packages/crashplan/
+## This script should be run once to populate a data-only container where Crashplan is installed:
+##  docker run --rm -it --volumes-from crashplan_data -v $(pwd):/home ubuntu /home/crashplan-install.sh
+## It also runs the crashplan image one time to generate a my.service.xml file which will be automatically updated with the Docker IP address each time the crashplan container is started.
+volume_name=crashplan_data
 pkgname=crashplan
 pkgver=3.6.3
 srcdir=/
 
-cd $srcdir/CrashPlan-install
+cd "$srcdir"
+## Download and extract Crashplan
+apt-get update && apt-get -qy install wget
+wget "http://download.crashplan.com/installs/linux/install/CrashPlan/CrashPlan_""$pkgver""_Linux.tgz" -O crashplan.tar.gz
+tar zxf crashplan.tar.gz
+rm crashplan.tar.gz
+
+## Setup initial variables for installation
+cd "$srcdir/CrashPlan-install"
 
 echo ""
 echo "You must review and agree to the EULA before using Crashplan."
@@ -14,7 +26,7 @@ echo "  - /usr/share/licenses/${pkgname}/LICENSE"
 echo ""
 
 echo "" > install.vars
-echo "JAVACOMMON=`which java`" >> install.vars
+echo "JAVACOMMON=/usr/bin/java" >> install.vars
 echo "#APP_BASENAME=CrashPlan" >> install.vars
 echo "TARGETDIR=/opt/$pkgname" >> install.vars
 echo "BINSDIR=" >> install.vars
@@ -28,14 +40,11 @@ sed -imod "s|Exec=.*|Exec=/opt/$pkgname/bin/CrashPlanDesktop|" $srcdir/CrashPlan
 sed -imod "s|Icon=.*|Icon=/opt/$pkgname/skin/icon_app_64x64.png|" $srcdir/CrashPlan-install/scripts/CrashPlan.desktop
 sed -imod "s|Categories=.*|Categories=System;|" $srcdir/CrashPlan-install/scripts/CrashPlan.desktop
 
-mkdir -p /opt/$pkgname
 cd /opt/$pkgname
 
 cat $srcdir/CrashPlan-install/CrashPlan_$pkgver.cpi | gzip -d -c - | cpio -i --no-preserve-owner
 chmod 777 /opt/$pkgname/log
 sed -i "s|<manifestPath>manifest</manifestPath>|<manifestPath>/opt/$pkgname/manifest</manifestPath>|g" /opt/$pkgname/conf/default.service.xml
-
-ln -s "/opt/$pkgname/bin/CrashPlanDesktop" /usr/bin/CrashPlanDesktop
 
 # Fix for encoding troubles (CrashPlan ticket 178827)
 # Make sure the daemon is running using the same localization as
@@ -49,4 +58,3 @@ install -D -m 644 $srcdir/CrashPlan-install/EULA.txt "/usr/share/licenses/${pkgn
 install -D -m 755 $srcdir/CrashPlan-install/scripts/CrashPlanDesktop /opt/$pkgname/bin/CrashPlanDesktop
 install -D -m 644 $srcdir/CrashPlan-install/scripts/run.conf /opt/$pkgname/bin/run.conf
 install -D -m 755 $srcdir/CrashPlan-install/scripts/CrashPlanEngine /opt/$pkgname/bin/CrashPlanEngine
-install -D -m 755 $srcdir/CrashPlan-install/scripts/CrashPlan.desktop /usr/share/applications/crashplan.desktop

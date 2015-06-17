42cd491eb6f89d0cc50297d079cfab1fdfa985c6
Scott Hansen <firecat4153@gmail.com>
2015-06-17 14:16:32 -0700

    Small Samba bugfix

--- samba/initialize.sh
+++ samba/initialize.sh
@@ -3,6 +3,6 @@
 # smb.conf and users.txt (optional) from the current directory. Also use this to
 # update existing smb.conf and/or users.txt.
 docker run -v /config --name samba_config myscratch true &> /dev/null
-docker run -it --rm --volumes-from samba_config \
+docker run --rm --volumes-from samba_config \
     -v $(pwd):/home ubuntu:trusty \
     bash -c "cp /home/{smb.conf,users.txt} /config/ 2>/dev/null || :"

9ed3f40f2c160fc7bc1c3a3099a4fa29c4bb0938
Scott Hansen <firecat4153@gmail.com>
2016-05-14 20:44:13 -0700

    Updated samba to use Ubuntu 16.04 base and for Docker best practices.

--- samba/initialize.sh
+++ samba/initialize.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-# Creates the samba_config data-only container and populates it with the
-# smb.conf and users.txt (optional) from the current directory. Also use this to
-# update existing smb.conf and/or users.txt.
-docker run -v /config --name samba_config myscratch true &> /dev/null
-docker run --rm --volumes-from samba_config \
-    -v $(pwd):/home ubuntu:trusty \
-    bash -c "cp /home/{smb.conf,users.txt} /config/ 2>/dev/null || :"
+# Creates the samba_config data-only container (if it doesn't already exist) and
+# populates it with the smb.conf and users.txt (optional) from the current
+# directory. Also use this to update existing smb.conf and/or users.txt.
+docker create -v /config --name samba_config ubuntu 2>/dev/null || :
+docker cp smb.conf samba_config:/config/ 2>/dev/null || :
+docker cp users.txt samba_config:/config/ 2>/dev/null || :

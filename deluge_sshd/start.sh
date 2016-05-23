19e2604748174261906002d456a82f59151504ba
Scott Hansen <firecat4153@gmail.com>
2016-05-22 21:02:03 -0700

    Yet another Minor bugfix for deluge_sshd

--- deluge_sshd/start.sh
+++ deluge_sshd/start.sh
@@ -5,9 +5,9 @@
 
 pipework --wait
 
-cp /config/deluge_ed25519.pub /etc/ssh/$DELUGE_USER
-chown $DELUGE_USER /etc/ssh/$DELUGE_USER
-chmod 600 /etc/ssh/$DELUGE_USER
+cp /config/deluge_ed25519.pub /etc/ssh/$DELUGE_SSHD_USER
+chown $DELUGE_SSHD_USER /etc/ssh/$DELUGE_SSHD_USER
+chmod 600 /etc/ssh/$DELUGE_SSHD_USER
 
 mkdir /var/run/sshd
 /usr/sbin/sshd

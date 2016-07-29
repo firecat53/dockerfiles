b6b5716e1672a11e9ef23c52e331ccf1159c2227
Scott Hansen <firecat4153@gmail.com>
2016-07-29 09:36:32 -0700

    Update syncthing_discovery update_release method. Update Syncthing and Syncthing Discovery to v0.14.3

--- syncthing_discovery/update_release.py
+++ syncthing_discovery/update_release.py
@@ -12,57 +12,35 @@ except ImportError:
 
 
 def get_release():
-    """Get syncthing latest release version from the Github API.
+    """Get stdiscosrv latest linux-amd64 release version from the Jenkins API.
 
-    Returns: version - empty or string like 'v0.9.15'
+    Returns: download url
 
     """
-    github_url = ("https://api.github.com/repos/syncthing/"
-                  "syncthing/releases?per_page=1")
-    res = urlopen(github_url)
+    jenkins_url = ("https://build.syncthing.net/job/"
+                   "stdiscosrv/lastStableBuild/api/json")
+    res = urlopen(jenkins_url)
     if not res:
         return ""
     res = res.read().decode()
     res = json.loads(res)
-    if res[0]['prerelease'] is False:
-        return res[0]['name'] or res[0]['tag_name']
-    else:
-        print("Prerelease found. No changes made")
-        return ""
-
-
-def get_sha():
-    """Get most recent 'master' branch syncthing SHA from the Github API.
-
-    Returns: 7 digit SHA string
-
-    """
-    github_url = ("https://api.github.com/repos/syncthing/"
-                  "syncthing/commits?per_page=1&sha=master")
-    res = urlopen(github_url)
-    if not res:
-        return ""
-    res = res.read().decode()
-    res = json.loads(res)
-    return res[0]['sha'][:7]
+    fn = [i['fileName'] for i in res['artifacts']
+          if 'linux-amd64' in i['fileName']][0]
+    return "{}artifact/{}".format(res['url'], fn)
 
 
 def update_stdiscosrv():
     """Update stdicosrv Dockerfile with latest release download link
 
     """
-    v = get_release()
-    sha = get_sha()
-    v = get_release()
-    if not v:
+    url = get_release()
+    if not url:
         return
-    url = "https://build.syncthing.net/job/stdiscosrv/lastStableBuild/artifact/stdiscosrv-linux-amd64-{}+1-g{}.tar.gz"
     with open('Dockerfile') as f:
         file = f.readlines()
         for idx, line in enumerate(file):
             if line.startswith('ADD'):
-                file[idx] = "ADD {} /stdiscosrv.tar.gz\n".format(
-                    url.format(v, sha))
+                file[idx] = "ADD {} /stdiscosrv.tar.gz\n".format(url)
     with open('Dockerfile', 'w') as f:
         f.writelines(file)
 

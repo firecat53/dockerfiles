6789da2b4c4f7079e78cc8912b701ede951b42aa
Scott Hansen <firecat4153@gmail.com>
2017-06-03 16:41:10 -0700

    Add umask to deluge_sshd

--- deluge_sshd/initialize.sh
+++ deluge_sshd/initialize.sh
@@ -6,7 +6,7 @@ USER_ID=22000
 [ -f deluge_ed25519 ] || ssh-keygen -t ed25519 -N '' -q -f deluge_ed25519
 
 docker run -v /config \
-    -v $(pwd):/mnt/ \
+    -v "$(pwd):/mnt/" \
     --name deluge_sshd_config \
     ubuntu bash -c "cp -a /mnt/private/* /mnt/deluge_ed25519.pub /mnt/config.ini /config/"
 docker run --rm --volumes-from deluge_sshd_config ubuntu chown -R $USER_ID:users /config

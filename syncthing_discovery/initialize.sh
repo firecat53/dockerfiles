346a6a2c0b38e1bc2fa56cbdd8251e2ba36b6120
Scott Hansen <firecat4153@gmail.com>
2017-06-08 16:20:30 -0700

    Correct permissions for generated cert.pem and key.pem

--- syncthing_discovery/initialize.sh
+++ syncthing_discovery/initialize.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Generates cert.pem and key.pem using existing Syncthing image
-docker run --rm -v $(pwd):/mnt syncthing /syncthing -generate=/mnt
+docker run --user root --rm -v $(pwd):/mnt syncthing /syncthing -generate=/mnt
+chmod 644 *.pem
 rm ./config.xml

a5b6916f0df9541aa49b1f541cde73341090abcf
Scott Hansen <firecat4153@gmail.com>
2018-04-19 14:08:15 -0700

    Ensure bind address is updated for deluge and transmission

--- deluge/start.sh
+++ deluge/start.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 echo "::10" > /config/auth
-sleep 20 && \
+sleep 60 && \
     deluge-console -c /config \
-    "config -s listen_ports ($(cat /var/run/pia_port/pia_port), $(cat /var/run/pia_port/pia_port))" &
+    "config -s listen_ports ($(cat /var/run/pia_port/pia_port), $(cat /var/run/pia_port/pia_port))" && \
+    deluge-console -c /config \
+    "config -s listen_interface $(ip addr show tun0 | grep inet | awk '{print $2}')" &
 deluge-web --fork --config /config
 deluged -d -L info --config /config
--- transmission/start.sh
+++ transmission/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 [ ! -f /config/settings.json ] && cp /var/lib/transmission/settings.json /config/
-sleep 20 && transmission-remote --port "$(cat /var/run/pia_port/pia_port)" &
-exec transmission-daemon -f -g /config
+sleep 60 && transmission-remote --port "$(cat /var/run/pia_port/pia_port)" &
+exec transmission-daemon -f -g /config -i "$(ip addr show tun0|grep inet|awk '{print $2}')"

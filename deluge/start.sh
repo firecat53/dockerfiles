bc8f24de2e06d31bb8d23d7ee54291416ec84211
Scott Hansen <firecat4153@gmail.com>
2018-11-26 20:42:46 -0800

    Bugfix in shell script. Different ip route get output from alpine to ubuntu.

--- deluge/start.sh
+++ deluge/start.sh
@@ -4,7 +4,7 @@ umask 002
 echo "::10" > /config/auth
 sleep 60 && \
     deluge-console -c /config \
-        "config -s listen_interface $(ip route get 1 | awk '{print $NF;exit}')" && \
+        "config -s listen_interface $(ip route get 1 | awk '{print $7;exit}')" && \
     [ -e /var/run/pia/pia_port ] && \
         deluge-console -c /config \
             "config -s listen_ports ($(cat /var/run/pia/pia_port), $(cat /var/run/pia/pia_port))" &

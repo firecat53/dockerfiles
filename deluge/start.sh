be783fb0051bb435a0924a75e209dbd8439096d3
Scott Hansen <firecat4153@gmail.com>
2020-10-23 12:16:16 -0700

    Another deluge bugfix

--- deluge/start.sh
+++ deluge/start.sh
@@ -2,6 +2,6 @@
 
 umask 002
 listen_ip="$(ip route get 1 | awk '{print $5;exit}')"
-sed -ie "s|\(\"listen_interface\": \).*|\"listen_interface\": \"'$listen_ip'\",|" /config/core.conf
+sed -ie "s|\(\"listen_interface\": \).*|\"listen_interface\": \"$listen_ip\",|" /config/core.conf
 deluge-web -L info --config /config
 deluged -d -L info --config /config

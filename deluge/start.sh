5a79d1e21ff8208c140c087101d1697f544d3094
Scott Hansen <firecat4153@gmail.com>
2020-10-23 11:20:58 -0700

    Bugfix in deluge start.sh listen address

--- deluge/start.sh
+++ deluge/start.sh
@@ -3,6 +3,6 @@
 umask 002
 sleep 10 && \
     deluge-console -c /config \
-        "config --set listen_interface $(ip route get 1 | awk '{print $7;exit}')" && \
+        "config --set listen_interface $(ip route get 1 | awk '{print $5;exit}')" && \
 deluge-web -L info --config /config
 deluged -d -L info --config /config

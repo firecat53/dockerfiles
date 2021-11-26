358aa219d5a7e78eeeb793cb0d66cf2c38da5127
Scott Hansen <firecat4153@gmail.com>
2021-11-26 13:46:59 -0800

    Yet another attempt at fixing Deluge

--- deluge/start.sh
+++ deluge/start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 umask 002
-listen_ip="$(ip route get 1 | awk '{print $7;exit}')"
+listen_ip="$(ip route get 1 | awk '{print $5;exit}')"
 sed -ie "s|\(\"listen_interface\": \).*|\"listen_interface\": \"$listen_ip\",|" /config/core.conf
 deluge-web -L info --config /config
 deluged -d -L info --config /config
